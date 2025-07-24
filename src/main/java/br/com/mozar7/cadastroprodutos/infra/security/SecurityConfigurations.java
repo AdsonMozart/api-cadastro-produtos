f180b66c3591ae7aa4943b3237641d1cb8617fa9
Adson Mozart Santos Paixão <adson.santos@biti9.com.br>
2025-07-24 03:30:10 -0300

    feat:add securityfilterchain, config authorizeHttpRequests and fix table users in migrations

---
+++ src/main/java/br/com/mozar7/cadastroprodutos/infra/security/SecurityConfigurations.java
@@ -0,0 +1,29 @@
+package br.com.mozar7.cadastroprodutos.infra.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfigurations {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "/product").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+
+                .build();
+    }
+}
